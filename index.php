5d3f4fabc78b7a0b19d499cacdbdcb76928c7b55
HuyQuangNguyen <huyquangng258@gmail.com>
2024-06-20 23:25:11 +0700

    first commit 2

--- index.php
+++ index.php
@@ -8,13 +8,11 @@ $username = "php_docker";
 $password = "password";
 $dbname = "qualitycompany";
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo " ";
-} catch(PDOException $e) {
-    echo " " . $e->getMessage();
-}
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo " ";
+
 
 $error_message = ""; // Biến lưu trữ thông báo lỗi
 
